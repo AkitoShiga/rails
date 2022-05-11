c68d7165b640ee886025fee2675208cd3cbd3826
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2022-05-11 22:08:25 +0900

    staff login

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root "top#index"
     get "login"      => "sessions#new", as: :login
+    post "session"   => "session#create", as: :session
     delete "session" => "sessions#destroy"
   end
 
