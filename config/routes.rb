7a0b4817e9df75ba64d1efdb458a89bb1efbea74
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2022-05-23 00:53:31 +0900

    チャプター 10

--- config/routes.rb
+++ config/routes.rb
@@ -27,14 +27,16 @@ Rails.application.routes.draw do
       #delete "session" => "sessions#destroy"
 
       resource :session, only: [:create, :destroy]
+=begin
       get    "staff_members"          => "staff_members#index", as: :admin_staff_members
-      get    "staff_members/:id"      => "staff_members#show",  as: :admin_staff_member
       get    "staff_members/new"      => "staff_members#new",   as: :new_admin_staff_member
+      get    "staff_members/:id"      => "staff_members#show",  as: :admin_staff_member
       get    "staff_members/:id/edit" => "staff_members#edit",  as: :edit_admin_staff_member
       post   "staff_members"          => "staff_members#create"
       patch  "staff_members/:id"      => "staff_members#update"
       delete "staff_members/:id"      => "staff_members#destroy"
-      #resources :staff_members
+=end
+      resources :staff_members
     end
   end
 
